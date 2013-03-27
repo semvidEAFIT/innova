81cb1e32e4d0798a32c254171988fee9f3c21744
Juan Camilo Acosta <kmilo2093@gmail.com>
2013-03-27 10:14:50 -0500

    Crowd casi

--- innovaUnity/Assets/Scripts/Player.cs
+++ innovaUnity/Assets/Scripts/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour {
 	private float move;
 	private int countCrashed;
 	private float speed; 
+	private bool translate;
 	// Use this for initialization
 	void Start () {
-		move = 0.0001f;
+		move = 0.00001f;
 		countCrashed = 0;
 		speed = GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelCtrl>().maxGameSpeed;
+		translate=false;
 	}
 	
 	// Update is called once per frame
@@ -27,9 +29,15 @@ public class Player : MonoBehaviour {
 			Crowd c=(Crowd)GameObject.Find("Girl(Clone)").GetComponent("Crowd");
 			c.accelerateCrowd();
 		}
-		while(transform.position.x > posX-15){
-				transform.Translate(0.01f, 0, 0);
+			float posX = transform.position.x;
+			while(transform.position.x > posX-15f && translate){
+			
+				transform.Translate(speed, 0, 0);
+				Debug.Log("velocidad"+speed);
+				Debug.Log("posX"+posX);
+				Debug.Log("position now"+transform.position);
 			}
+		translate=false;
 	}
 	
 	void OnTriggerEnter(Collider c){
@@ -37,10 +45,7 @@ public class Player : MonoBehaviour {
 		if (c.tag == "Obstacle"){
 			//destroy obstacle, reset jumpcounter, get closer to crowd
 			countCrashed++;
-			float posX = transform.position.x;
-			
-			
-			
+			translate=true;
 			
 		}
 		if (c.tag == "Segway"){
